394709a33d2e58881922d75d89090aea38f6e905
Hamza Sher <hamzasher223@gmail.com>
2021-06-28 20:30:07 +0500

    Prepared for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,6 +155,7 @@ USE_TZ = True
 # STATIC_ROOT = [
 #     (os.path.join(PROJECT_ROOT, 'staticfiles'))
 # ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [(os.path.join(PROJECT_ROOT, 'static')),]
